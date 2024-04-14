459265a1b09c373a0adc2a22daeacde0e8174193
Svetlana Plinda <sv.plinda@icloud.com>
2024-04-14 21:46:16 +0200

    feat: solve logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -177,8 +177,19 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsString = args.map((arg) => JSON.stringify(arg)).join(',');
+    const functionName = func.name;
+
+    logFunc(`${functionName}(${argsString}) starts`);
+
+    const result = func.apply(this, args);
+
+    logFunc(`${functionName}(${argsString}) ends`);
+
+    return result;
+  };
 }
 
 /**
